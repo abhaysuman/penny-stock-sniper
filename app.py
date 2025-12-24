f2dfcf6f42b72c004c760f2e1537eb7b429e4d20
Abhay Suman <abhaysuman@Abhays-MacBook-Pro.local>
2025-12-24 23:04:55 +0530

    Upgrade AI Logic: 2-day Target & RSI Slope

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from backtesting.lib import crossover
 import streamlit.components.v1 as components
 
 # --- CONFIG ---
-st.set_page_config(layout="wide", page_title="AI Infinity Scanner", page_icon="📈")
+st.set_page_config(layout="wide", page_title="Stock Algo", page_icon="📈")
 
 st.markdown("""
 <style>
